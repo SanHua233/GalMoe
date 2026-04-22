6cdf8f02a150096abe306dc5c52d655c0c112629
SanHuaOuO <d1612410303@outlook.com>
2026-04-22 15:51:55 +0800

    Stop tracking config.py, add to .gitignore

--- .gitignore/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    SECRET_KEY = "your_secret_key_here"
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "123456"
-    MYSQL_DB = "galmoe"
-
-    BANGUMI_API_BASE = "https://api.bgm.tv"
-    BANGUMI_API_TOKEN = "vTCCjoB4wSYVGa47yHZoJpnYWzsIeMvWF47kOnzY" 
